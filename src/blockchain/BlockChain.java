36423f0826f81635f742ca686b2198f403ad9a5a
Your Name <you@example.com>
2019-04-16 00:23:56 +0530

    reinitialized files

---
+++ src/blockchain/BlockChain.java
@@ -0,0 +1,96 @@
+package blockchain;
+import java.util.ArrayList;
+import java.util.*;
+import com.google.gson.GsonBuilder;
+
+public class BlockChain {
+	
+	public static ArrayList<Block> blockchain = new ArrayList<Block>();
+	public static int difficulty = 5;
+
+	public static void main(String[] args) {	
+		Scanner sc=new Scanner(System.in);  
+	        System.out.println("Enter choice \n1.Add a new block \n2.Check validity of blockchain \n3.Get data in JSON format \n4.Get blockchain size \n5.Exit");  
+	        int ch=sc.nextInt();  
+	        while (ch<6)
+	        {
+	        switch(ch)  
+	        {  
+	        case 1:  
+	        	System.out.println("Enter the data");  
+	    		Scanner in = new Scanner(System.in); 
+	    		String data = in.nextLine();
+	    		if(blockchain.size()>0)
+	    		{
+	    			addBlock(new Block(data,blockchain.get(blockchain.size()-1).hash));
+	    		}
+	    		else
+	    		{
+	    			addBlock(new Block(data, "0"));
+	    		}
+
+	    		
+	            break;  
+	              
+	        case 2:  
+	        	System.out.println("\nBlockchain is Valid: " + isChainValid()); 
+	            break;  
+	  
+	        case 3:  
+
+	    		String blockchainJson = StringUtil.getJson(blockchain);
+	    		System.out.println("\nThe block chain: ");
+	    		System.out.println(blockchainJson);
+	            break;  
+	  
+	        case 4:  
+	        	System.out.println("Blockchain size: "+blockchain.size());
+	            break;  
+	        
+	        case 5:  
+	        	System.exit(0);
+	            break;  
+	  
+	        default:  
+	            System.out.println("Invalid choice");  
+	        }
+	        System.out.println("\n");
+	        System.out.println("Enter choice \n1.Add a new block \n2.Check validity of blockchain \n3.Get data in JSON format \n4.Get blockchain size \n5.Exit");
+	        ch = sc.nextInt();
+	   }	
+        }
+	
+	public static Boolean isChainValid() {
+		Block currentBlock; 
+		Block previousBlock;
+		String hashTarget = new String(new char[difficulty]).replace('\0', '0');
+		
+		//loop through blockchain to check hashes:
+		for(int i=1; i < blockchain.size(); i++) {
+			currentBlock = blockchain.get(i);
+			previousBlock = blockchain.get(i-1);
+			//compare registered hash and calculated hash:
+			if(!currentBlock.hash.equals(currentBlock.calculateHash()) ){
+				System.out.println("Current Hashes not equal");			
+				return false;
+			}
+			//compare previous hash and registered previous hash
+			if(!previousBlock.hash.equals(currentBlock.previousHash) ) {
+				System.out.println("Previous Hashes not equal");
+				return false;
+			}
+			//check if hash is solved
+			if(!currentBlock.hash.substring( 0, difficulty).equals(hashTarget)) {
+				System.out.println("This block hasn't been mined");
+				return false;
+			}
+			
+		}
+		return true;
+	}
+	
+	public static void addBlock(Block newBlock) {
+		newBlock.mineBlock(difficulty);
+		blockchain.add(newBlock);
+	}
+}
