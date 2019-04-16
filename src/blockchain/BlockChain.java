b5158ede9c9083f46aeaf6d7374c820a9736bbaf
pshgpt <32108574+pshgpt@users.noreply.github.com>
2019-04-16 16:01:47 +0530

    Update BlockChain.java

--- src/blockchain/BlockChain.java
+++ src/blockchain/BlockChain.java
@@ -8,11 +8,12 @@ public class BlockChain {
 	public static ArrayList<Block> blockchain = new ArrayList<Block>();
 	public static int difficulty = 5;
 
-	public static void main(String[] args) {	
+	public static void main(String[] args) {
+		String str=null;
 		Scanner sc=new Scanner(System.in);  
-	        System.out.println("Enter choice \n1.Add a new block \n2.Check validity of blockchain \n3.Get data in JSON format \n4.Get blockchain size \n5.Exit");  
+	        System.out.println("Enter choice \n1.Add a new block \n2.Check validity of blockchain \n3.Get data in JSON format \n4.Get blockchain size \n5.Search the data \n6.Exit");  
 	        int ch=sc.nextInt();  
-	        while (ch<6)
+	        while (ch<7)
 	        {
 	        switch(ch)  
 	        {  
@@ -29,7 +30,6 @@ public class BlockChain {
 	    			addBlock(new Block(data, "0"));
 	    		}
 
-	    		
 	            break;  
 	              
 	        case 2:  
@@ -46,8 +46,14 @@ public class BlockChain {
 	        case 4:  
 	        	System.out.println("Blockchain size: "+blockchain.size());
 	            break;  
-	        
 	        case 5:  
+	        	System.out.println("Enter the data to be searched");
+
+	        	 str=sc.next();
+   		 search(str);
+	        
+	            break;  
+	        case 6:  
 	        	System.exit(0);
 	            break;  
 	  
@@ -55,10 +61,28 @@ public class BlockChain {
 	            System.out.println("Invalid choice");  
 	        }
 	        System.out.println("\n");
-	        System.out.println("Enter choice \n1.Add a new block \n2.Check validity of blockchain \n3.Get data in JSON format \n4.Get blockchain size \n5.Exit");
+	        System.out.println("Enter choice \n1.Add a new block \n2.Check validity of blockchain \n3.Get data in JSON format \n4.Get blockchain size \n5.Search the data \n6.Exit");
 	        ch = sc.nextInt();
 	   }	
         }
+	public static void search(String str) {
+		for (int i=0; i<blockchain.size();i++) {
+			if(blockchain.get(i).getData().equals(str))
+			{
+				System.out.println("Data: "+blockchain.get(i).getData());
+				System.out.println("Hash Value: "+blockchain.get(i).getHash());
+				System.out.println("Previous Hash: "+blockchain.get(i).getPreviousHash());
+				System.out.println("TimeStamp: "+blockchain.get(i).getTimeStamp());
+				System.out.println("Nonce: "+blockchain.get(i).getNonce());
+				
+			}
+			else {
+				System.out.println("Invalid Search");
+			}
+		}
+			
+				
+		}
 	
 	public static Boolean isChainValid() {
 		Block currentBlock; 
